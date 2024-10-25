78e58e571ce10d0b0746049362e6e83fd1e97028
Ron Peters <rpeters@sendhub.com>
2024-10-25 10:38:55 -0230

    migration

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
-# this is not a love song
-# http://packages.python.org/distribute/setuptools.html
-# http://diveintopython3.org/packaging.html
-# http://wiki.python.org/moin/CheeseShopTutorial
-# http://pypi.python.org/pypi?:action=list_classifiers
-
-from ez_setup import use_setuptools
-use_setuptools(version="0.6c11")
+# setup.py for redisbayes, updated for Python 3.11 compatibility
 
 import os
 from setuptools import setup
@@ -15,21 +8,25 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name                 = "redisbayes",
-    version              = __import__("redisbayes").__version__,
-    description          = u"Naïve Bayesian Text Classifier on Redis",
-    long_description     = read("README.md"),
-    author               = "Justine Tunney",
-    author_email         = "jtunney@gmail.com",
-    url                  = "https://github.com/jart/redisbayes",
-    license              = "MIT",
-    install_requires     = ["redis==3.5.3"],
-    py_modules           = ["redisbayes", "ez_setup"],
-    classifiers = [
+    name="redisbayes",
+    version="1.0.0",  # Alternatively, import version from the module if defined there
+    description=u"Naïve Bayesian Text Classifier on Redis",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",  # If README.md is Markdown
+    author="Justine Tunney",
+    author_email="jtunney@gmail.com",
+    url="https://github.com/jart/redisbayes",
+    license="MIT",
+    install_requires=[
+        "redis>=4.0.0"  # Update redis to a compatible version for Python 3.11
+    ],
+    py_modules=["redisbayes"],
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
-        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Database",
         "Topic :: Communications :: Email",
     ],
